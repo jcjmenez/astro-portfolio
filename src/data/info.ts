51c061fd3594ecce23bf06bf312a0d01e58bfbfc
jcjmenez <jcjmenez.02@gmail.com>
2023-03-13 22:00:45 +0100

    first commit

---
+++ src/data/info.ts
@@ -0,0 +1,51 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Juan Carlos Jiménez",
+  jobDescription: "Web Developer",
+  about: `Soy Juan Carlos, estudiante de ingeniería informática en la Universidad Europea.
+  Me considero una persona responsable, me apasiona resolver problemas, trabajar en equipo y gestionar el tiempo de una manera proactiva.
+  Mi meta profesional es trabajar en un entorno en el que se crea en la innovación y sea creativo.
+  `,
+  
+  experience: [
+    /*
+    {
+      name: "Universidad Europea",
+      location: "Madrid, España",
+      startDate: "Sept 2020",
+      endDate: "Present",
+      description: [
+        "- Estudiante en el tercer año del grado en Ingeniería Informática por la Universidad Europea de Madrid.",
+      ],
+    }
+    */
+  ],
+  
+  education: [
+    {
+      name: "Universidad Europea",
+      location: "Madrid, España",
+      startDate: "Sept 2020",
+      endDate: "Present",
+      description: [
+        "- Estudiante en el tercer año del grado en Ingeniería Informática por la Universidad Europea de Madrid.",
+      ],
+    }
+  ],
+
+  socialMedia: {
+    github: "https://github.com/jcjmenez",
+    email: "jcjmenez.02@gmail.com",
+    linkedin: "https://www.linkedin.com/in/jcjmenez/",
+  },
+
+  projects: [
+    {
+      title: "Astro Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+  ],
+};
